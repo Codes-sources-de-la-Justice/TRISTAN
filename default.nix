a918e20184682edadb8527ed906b68ba1a92da80
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2021-12-06 18:18:56 +0100

    ci: add frontend test

--- default.nix
+++ default.nix
@@ -10,6 +10,48 @@ let
   frontend = callPackage ./nix/frontend.nix {
     src = filteredSrc;
   };
+  mkTristanEnv = { copyNodeModules ? false }:
+  pkgs.npmlock2nix.shell {
+      src = filteredSrc;
+      buildInputs = with pkgs; [
+        # Python's backend
+        backend.env
+
+        # Node.js
+        nodejs
+        yarn2nix
+        yarn
+
+        # jq-like tool for XML
+        yq
+
+        # Development app/worker runner
+        foreman
+        # Docker-Compose on steroids
+        arion
+        docker-client # Required when using podman
+
+        # For SPS mock
+        nodePackages.json-server
+
+        # Vulnerabilities assessments
+        nodePackages.snyk
+      ];
+
+      # Ensure Puppeteer can find a Chromium browser.
+      PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium.outPath}/bin/chromium";
+      # Do not symlink the initial node_modules, otherwise, it will not be possible to interactively edit the package(-lock).json.
+      node_modules_mode = if copyNodeModules then "copy" else "symlink";
+      node_modules_attrs = {
+        PUPPETEER_SKIP_DOWNLOAD = "true";
+      };
+      # Use common settings.
+      DJANGO_SETTINGS_MODULE = "common.settings";
+      # Development shell is in debug.
+      DEBUG = "True";
+      # TRISTAN frontend
+      TRISTAN_FRONTEND = "http://localhost:3000";
+    };
 in rec {
   inherit backend frontend;
   dev-containers = pkgs.arion.build {
@@ -18,45 +60,6 @@ in rec {
   };
   ci = [ docker.backend-app docker.backend-worker frontend.production-bundle dev-containers ];
   docker = callPackage ./nix/docker.nix {};
-  shell = pkgs.npmlock2nix.shell {
-    src = filteredSrc;
-    buildInputs = with pkgs; [
-      # Python's backend
-      backend.env
-
-      # Node.js
-      nodejs
-      yarn2nix
-      yarn
-
-      # jq-like tool for XML
-      yq
-
-      # Development app/worker runner
-      foreman
-      # Docker-Compose on steroids
-      arion
-      docker-client # Required when using podman
-
-      # For SPS mock
-      nodePackages.json-server
-
-      # Vulnerabilities assessments
-      nodePackages.snyk
-    ];
-
-    # Ensure Puppeteer can find a Chromium browser.
-    PUPPETEER_EXECUTABLE_PATH = "${pkgs.chromium.outPath}/bin/chromium";
-    # Do not symlink the initial node_modules, otherwise, it will not be possible to interactively edit the package(-lock).json.
-    node_modules_mode = "copy";
-    node_modules_attrs = {
-      PUPPETEER_SKIP_DOWNLOAD = "true";
-    };
-    # Use common settings.
-    DJANGO_SETTINGS_MODULE = "common.settings";
-    # Development shell is in debug.
-    DEBUG = "True";
-    # TRISTAN frontend
-    TRISTAN_FRONTEND = "http://localhost:3000";
-  };
+  shell = mkTristanEnv { copyNodeModules = true; };
+  ciShell = mkTristanEnv { copyNodeModules = true; }; # Patch vite-jest
 }
--- src/frontend/App.test.jsx
+++ src/frontend/App.test.jsx
@@ -1,8 +1,8 @@
 import { render, screen } from "@testing-library/react";
+import React from 'react';
 import App from "./App";
 
 test("renders home page", () => {
   render(<App />);
-  const linkElement = screen.getByText(/liste des affaires/i);
-  expect(linkElement).toBeInTheDocument();
+	expect(screen.queryAllByText(/liste des affaires/i).length).toBeGreaterThan(0);
 });
