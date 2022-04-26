538ff403a859400113f1a103b5d653c6e1c3bc08
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2022-04-26 14:53:00 +0200

    refactor(summary): minor fixes, missing imports

--- src/frontend/components/DemoSummary.jsx
+++ src/frontend/components/DemoSummary.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import Summary from '../containers/Summary.jsx';
-import { db, toBackendPayload, toGraph } from '../static';
+import Summary from '../containers/Summary';
+import { toBackendPayload, toGraph } from '../static';
+import { db } from '../static/db';
+
+class ErrorBoundary extends React.Component {
+  state = {
+    hasError: false
+  };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error('Error', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <h1>Une erreur fatale est survenue, veuillez recharger TRISTAN.</h1>
+      );
+    }
+
+    return this.props.children;
+  }
+}
 
 function DemoSummary({databaseKey}) {
 	const payload = toBackendPayload(db[databaseKey]);
 	const { elements } = toGraph(payload);
 	const { facts, victims, indictees, general } = payload;
+
 	// TODO: add witnesses, others.
 	return (
-		<Summary
+    <ErrorBoundary>
+	<Summary
 			elements={elements}
 			summary={{
 				entities: {
@@ -19,7 +50,9 @@ function DemoSummary({databaseKey}) {
 				general: general
 			}}
 		/>
-	);
+
+    </ErrorBoundary>
+		);
 }
 
 export default DemoSummary;
--- src/frontend/containers/Summary.tsx
+++ src/frontend/containers/Summary.tsx
@@ -1,124 +1,162 @@
 import React, { useState, useCallback } from "react";
 
-import { getClosedNeighborWithDepth } from '../static';
-import Joyride from 'react-joyride'
+import { getClosedNeighborWithDepth } from "../static";
+import Joyride from "react-joyride";
 
-import { FactCard, GridCard, PersonCard } from '../components/cards';
+import { FactInfo } from '../components/cards/Fact';
+import { PersonInfo } from '../components/cards/Person';
+import { FactCard, GridCard, PersonCard } from "../components/cards";
 
-import './Summary.css';
-import {Fact, GeneralInformation, PersonEntityPartition} from "static/model";
-import { ElementDefinition } from 'cytoscape'
+import "./Summary.css";
+import { Fact, GeneralInformation, PersonEntityPartition } from "static/model";
+import { ElementDefinition } from "cytoscape";
 
 type SummaryProps = {
-	summary: {
-		entities: PersonEntityPartition;
-		facts: Fact[];
-		general: GeneralInformation;
-	};
-	elements: ElementDefinition[];
+  summary: {
+    entities: PersonEntityPartition;
+    facts: Fact[];
+    general: GeneralInformation;
+  };
+  elements: ElementDefinition[];
 };
 
-function Sidebar({open, entity, onClose}) {
-	if (!open) return null;
+function Sidebar({ open, entity, onClose }) {
+  if (!open) return null;
 
-	const entityType = entity.Natinf != null ? "fact" : "person";
-	const entityInfo = entityType === "fact" ? (<FactInfo fact={entity} level={2} />) : (<PersonInfo person={entity} level={2} />);
+  const entityType = entity.Natinf != null ? "fact" : "person";
+  const entityInfo =
+    entityType === "fact" ? (
+      <FactInfo fact={entity} level={2} />
+    ) : (
+      <PersonInfo person={entity} level={2} />
+    );
 
-	return (
-		<div className="sidebar">
-			<button onClick={onClose}>Fermer</button>
-			{entityInfo}
-		</div>
-	);
+  return (
+    <div className="sidebar">
+      <button onClick={onClose}>Fermer</button>
+      {entityInfo}
+    </div>
+  );
 }
 
-function GeneralInfo({general: {Jonction, Depaysement, Scelles}, entities: { victims, indictees }, facts}) {
-	// S'il existe un mineur, le montrer
-	// Calculer atteintes aux biens -> NATAF?
-	return (
-		<>
-			{Jonction && <Tag>Jonction</Tag>}
-			{Depaysement && <Tag>Dépaysement</Tag>}
-			{Scelles && <Tag>Présence de scellés</Tag>}
-		</>
-	);
+function GeneralInfo({
+  general: { Jonction, Depaysement, Scelles },
+  entities: { victims, indictees },
+  facts,
+}) {
+  // S'il existe un mineur, le montrer
+  // Calculer atteintes aux biens -> NATAF?
+  return (
+    <>
+      {Jonction && <Tag>Jonction</Tag>}
+      {Depaysement && <Tag>Dépaysement</Tag>}
+      {Scelles && <Tag>Présence de scellés</Tag>}
+    </>
+  );
 }
 
-function Summary({summary: {entities, facts, general}, elements}: SummaryProps) {
-	const [selectedEntity, selectEntity] = useState(null);
-	const handleClose = useCallback(() => selectEntity(null));
 
-	const [preferredIds, setPreferredIds] = useState([]);
-	const hiddenIds = preferredIds.length > 0 ? [...Object.values(entities), facts].flatMap(a => a.filter(n => !preferredIds.includes(n.Global_Id)).map(n => n.Global_Id)) : [];
+function Summary({
+  summary: { entities, facts, general },
+  elements,
+}: SummaryProps) {
+  const [selectedEntity, selectEntity] = useState(null);
+  const handleClose = useCallback(() => selectEntity(null));
 
-	const handleEntityClick = entity => {
-		selectEntity(entity);
+  const [preferredIds, setPreferredIds] = useState([]);
+  const hiddenIds =
+    preferredIds.length > 0
+      ? [...Object.values(entities), facts].flatMap((a) =>
+          a
+            .filter((n) => !preferredIds.includes(n.Global_Id))
+            .map((n) => n.Global_Id)
+        )
+      : [];
 
-		if (Object.is(selectedEntity, entity) && preferredIds.length > 0) {
-			setPreferredIds([]);
-		} else {
-			const [_, neighbors] = getClosedNeighborWithDepth(entity.Global_Id, elements, 3)
-			setPreferredIds(Array.from(neighbors));
-		}
-	};
+  const handleEntityClick = (entity) => {
+    selectEntity(entity);
 
-	const steps = [
-		{
-			target: '.summary',
-			content: 'Bienvenue dans votre visualisation de synthèse',
-			placementBeacon: 'top'
-		},
-		{
-			target: '.entity-card',
-			content: 'Vous trouverez vos entités ici !',
-			placementBeacon: 'top',
-		}
-	];
+    if (Object.is(selectedEntity, entity) && preferredIds.length > 0) {
+      setPreferredIds([]);
+    } else {
+      const [_, neighbors] = getClosedNeighborWithDepth(
+        entity.Global_Id,
+        elements,
+        3
+      );
+      setPreferredIds(Array.from(neighbors));
+    }
+  };
 
-	const selectedId = preferredIds.length > 0 ? selectedEntity?.Global_Id : null;
+  const steps = [
+    {
+      target: ".summary",
+      content: "Bienvenue dans votre visualisation de synthèse",
+      placementBeacon: "top",
+    },
+    {
+      target: ".entity-card",
+      content: "Vous trouverez vos entités ici !",
+      placementBeacon: "top",
+    },
+  ];
 
-	return (
-		<div className="summary-container">
-			<Joyride
-				continuous
-				showSkipButton
-				showProgress
-				debug
-				locale={{
-					back: 'Retour',
-					close: 'Fermer',
-					last: 'Fin',
-					next: 'Suivant',
-					open: 'Ouvrir la modale',
-					skip: 'Passer'
-				}}
-				steps={steps} />
-			<div className="summary">
-				<div className="summary-header">
-					<h1>Procédure</h1>
-					<div className="summary-header__tags">
-						<GeneralInfo entities={entities} general={general} />
-					</div>
-				</div>
-				<div className="summary-body">
-					<GridCard Component={FactCard} entities={facts} onClick={handleEntityClick} hiddenIds={hiddenIds} selectedId={selectedId} />
-					<hr />
-					{Object.values(entities)
-							.map((subentities, index) => (
-								<React.Fragment key={index}>
-									<GridCard
-										Component={PersonCard}
-										entities={subentities}
-										onClick={handleEntityClick} hiddenIds={hiddenIds} selectedId={selectedId} />
-									<hr />
-								</React.Fragment>
-									))
-					}
-				</div>
-			</div>
-			<Sidebar open={selectedEntity != null} entity={selectedEntity} onClose={handleClose} />
-		</div>
-	);
+  const selectedId = preferredIds.length > 0 ? selectedEntity?.Global_Id : null;
+
+  return (
+    <div className="summary-container">
+      <Joyride
+        continuous
+        showSkipButton
+        showProgress
+        debug
+        locale={{
+          back: "Retour",
+          close: "Fermer",
+          last: "Fin",
+          next: "Suivant",
+          open: "Ouvrir la modale",
+          skip: "Passer",
+        }}
+        steps={steps}
+      />
+      <div className="summary">
+        <div className="summary-header">
+          <h1>Procédure</h1>
+          <div className="summary-header__tags">
+            <GeneralInfo entities={entities} general={general} />
+          </div>
+        </div>
+        <div className="summary-body">
+          <GridCard
+            Component={FactCard}
+            entities={facts}
+            onClick={handleEntityClick}
+            hiddenIds={hiddenIds}
+            selectedId={selectedId}
+          />
+          <hr />
+          {Object.values(entities).map((subentities, index) => (
+            <React.Fragment key={index}>
+              <GridCard
+                Component={PersonCard}
+                entities={subentities}
+                onClick={handleEntityClick}
+                hiddenIds={hiddenIds}
+                selectedId={selectedId}
+              />
+              <hr />
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+      <Sidebar
+        open={selectedEntity != null}
+        entity={selectedEntity}
+        onClose={handleClose}
+      />
+    </div>
+  );
 }
 
 export default Summary;
