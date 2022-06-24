bd673265231803b15a2f6ac5fa647c9d4770f270
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2022-06-24 15:42:43 +0200

    tgcm: remove two files

--- libs/tristan-core/src/static/tgcm/index.ts
+++ libs/tristan-core/src/static/tgcm/index.ts
@@ -1,5 +1,3 @@
 export const files = [
-  ["TGCM_aleatoire_15x8.json", import("./TGCM_aleatoire_15x8.json")],
-  ["TGCM_aleatoire_bcp_faits.json", import("./TGCM_aleatoire_bcp_faits.json")],
-  ["TGCM_aleatoire_dense.json", import("./TGCM_aleatoire_dense.json")],
+  ["TGCM_aleatoire_15x8.json", import("./TGCM_aleatoire_15x8.json")]
 ];
