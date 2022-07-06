d58be354004eeffbd982e3b193e50379666b23a3
Ryan LAHFA <ryan.lahfa@justice.gouv.fr>
2022-07-06 15:54:51 +0200

    tristan(standalone): use HashRouter for public demo

--- apps/tristan-standalone/src/app/app.tsx
+++ apps/tristan-standalone/src/app/app.tsx
@@ -1,72 +1,101 @@
-import "./app.module.css";
-
 import React, { Fragment } from "react";
-import { Route, Router } from "wouter";
-import { DefaultTheme } from 'styled-components';
+import {
+  HashRouter as Router,
+  Route,
+  Link,
+  Routes,
+  useParams,
+} from "react-router-dom";
+import { DefaultTheme } from "styled-components";
 
 // import { Header, HeaderNav, NavItem } from "@dataesr/react-dsfr"
 //import Joyride from "react-joyride"
 
-import { SchemaContainer, SummaryContainer, db } from "@codes-sources-de-la-justice/react-tristan";
+import {
+  SchemaContainer,
+  SummaryContainer,
+  db,
+} from "@codes-sources-de-la-justice/react-tristan";
 
-import 'remixicon/fonts/remixicon.css';
+import "remixicon/fonts/remixicon.css";
 
 function Demo() {
-	return (
-		<main>
-			<h1 className="App">Démonstration de TRISTAN</h1>
+  return (
+    <main>
+      <h1>Démonstration de TRISTAN</h1>
 
-			<section className="Affaires">
-				{Object.entries(db).map(([key, json]) => {
-					return (
-						<Fragment key={key}>
-							<a href={`/demo/summary/${key}`}>Visualisation de synthèse: {key}</a>
-							<a href={`/demo/schema/${key}`}>Schéma de synthèse: {key}</a>
-						</Fragment>
-					);
-				})}
-			</section>
-		</main>
-	);
+      <section>
+        {Object.entries(db).map(([key, json]) => {
+          return (
+            <Fragment key={key}>
+							<Link to={`/summary/${key}`}>
+                Visualisation de synthèse: {key}
+              </Link>
+							<Link to={`/schema/${key}`}>Schéma de synthèse: {key}</Link>
+            </Fragment>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 function Home() {
   return (
-		<main>
-			<h1 className="App">Shell TRISTAN standalone</h1>
-			<p>Cette application est le mode standalone de TRISTAN et permet de développer TRISTAN sans les adhérences aux autres applicatifs.</p>
-			<p>TRISTAN sait lire des résultats d'analyse statiques chargés (TODO autodetect), soit utiliser un backend vers une URL déjà configuée.</p>
-		</main>
+    <main>
+      <h1 className="App">Shell TRISTAN standalone</h1>
+      <p>
+        Cette application est le mode standalone de TRISTAN et permet de
+        développer TRISTAN sans les adhérences aux autres applicatifs.
+      </p>
+      <p>
+        TRISTAN sait lire des résultats d'analyse statiques chargés (TODO
+        autodetect), soit utiliser un backend vers une URL déjà configuée.
+      </p>
+      <Link to="demo">Aller à la demo</Link>
+    </main>
   );
 }
 
 const standaloneTheme: DefaultTheme = {
- 	standalone: true
+  standalone: true,
 };
 
+function ParametrizedContainer(shadowRoot: HTMLElement, theme: DefaultTheme, component: any) {
+  return function () {
+    const params = useParams();
+    return React.createElement(component, {
+      idj: params["key"],
+      shadowRoot,
+      theme,
+    });
+  };
+}
+
 function App() {
-	const head = document.documentElement;
-	const basePath = document.baseURI.split(window.location.origin)[1] || '/';
-
-	return (
-		<Router>
-			<Route path="/">
-				<Home />
-			</Route>
-
-			<Route path="/demo">
-				<Demo />
-			</Route>
-
-			<Route path="/demo/schema/:key">
-				{params => (<SchemaContainer idj={params?.key} shadowRoot={head} theme={standaloneTheme} />)}
-			</Route>
-
-			<Route path="/demo/summary/:key">
-				{params => (<SummaryContainer idj={params?.key} shadowRoot={head} theme={standaloneTheme} />)}
-			</Route>
-		</Router>
-	);
+  const head = document.documentElement;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+					<Route path="demo" element={<Demo />} />
+          <Route
+            path="schema/:key"
+            element={React.createElement(
+              ParametrizedContainer(head, standaloneTheme, SchemaContainer)
+            )}
+          />
+          <Route
+            path="summary/:key"
+            element={React.createElement(
+              ParametrizedContainer(head, standaloneTheme, SummaryContainer)
+            )}
+          />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
